396ab49b678ff368c8affa26d66701d0da8cdccc
Samir Aryamane <saryamane@users.noreply.github.com>
2019-08-15 17:17:42 -0700

    Formatting changes
    Basic changes to how the API return response is.

--- heroku_webhook.py
+++ heroku_webhook.py
@@ -41,7 +41,7 @@ def makeResponse(req):
         if date in weather[i]['dt_txt']:
             condition= weather[i]['weather'][0]['description']
             break
-    speech = "The forecast for"+city+ "for "+date+" is "+condition
+    speech = "The forecast for "+city+ " on "+date+" is "+condition
     return {
     "fulfillment_text": speech,
     "webhook_source": "heroku-weather-webhook"
